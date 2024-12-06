dc2f70a0d339260d82c752c834e4c00f33d5f997
Карев Александр Александрович ИП-414 <kruki999@yandex.ru>
2024-12-06 20:50:37 +0700

    реализованы 4 generic метода и написаны тесты

---
+++ src/GenericTest.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class GenericTest {
+    @Test
+    public <T> void GenericMethodsTest() {
+        //given
+        List<Integer> t1 = List.of(1, 2, 3, 4, 7, 8, 10);
+        List<Integer> t2 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Generics<Integer, Number> genericTest = new Generics<>();
+
+        //when
+        List<Integer> testList = genericTest.firstGeneric(t1, t2);
+
+        //then
+        Assertions.assertEquals("[1, 2, 3, 4, 7, 8]", testList.toString());
+    }
+
+    @Test
+    public void secondGenericTest() {
+        //given
+        List<Integer> t1 = List.of(1, 2, 3, 4, 7, 8, 10);
+        List<Integer> t2 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Generics<Integer, Number> testGeneric = new Generics<>();
+
+        //when
+        List<Integer> result = testGeneric.secondGeneric(t1, t2);
+
+        //then
+        Assertions.assertEquals("[10, 5, 6, 9]", result.toString());
+    }
+
+    @Test
+    public void testThirdGeneric() {
+        //given
+        List<Integer> t1 = List.of(1, 2, 3, 4, 7, 8, 10);
+        List<Integer> t2 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Generics<Integer, Number> testGeneric = new Generics<>();
+
+        //when
+        List<Integer> result = testGeneric.thirdGeneric(t1, t2);
+
+        //then
+        Assertions.assertEquals("[1, 2, 3, 4, 7, 8, 10, 5, 6, 9]", result.toString());
+    }
+
+    @Test
+    public void testFourthGeneric() {
+        //given
+        List<Integer> t2 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Predicate<Integer> isEven = number -> number % 2 != 0;
+        Generics testGeneric = new Generics<>();
+
+        //when
+        List<Integer> result = testGeneric.fourthGeneric(t2, isEven);
+
+        //then
+        Assertions.assertEquals("[1, 3, 5, 7, 9]", result.toString());
+    }
+}
---
+++ src/Generics.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.util.function.Predicate;
+
+public class Generics<T, I extends Number> {
+    public <T> List<T> firstGeneric(List<T> firstList, List<T> secondList) {
+        List<T> result = new ArrayList<>();
+        for (T t : firstList) {
+            for (T t2 : secondList) {
+                if (t.equals(t2)) {
+                    result.add(t);
+                }
+            }
+        }
+        return result;
+    }
+
+    public <T> List<T> secondGeneric(List<T> firstList, List<T> secondList) {
+        List<T> result = new ArrayList<>();
+        for (T t1 : firstList) {
+            if (!secondList.contains(t1)) {
+                result.add(t1);
+            }
+        }
+
+        for (T t2 : secondList) {
+            if (!firstList.contains(t2)) {
+                result.add(t2);
+            }
+        }
+        return result;
+    }
+
+    public <T> List<T> thirdGeneric(List<T> firstList, List<T> secondList) {
+        List<T> result = new ArrayList<>();
+        for (T t1 : firstList) {
+            result.add(t1);
+        }
+
+        for (T t2 : secondList) {
+            if (!result.contains(t2)) {
+                result.add(t2);
+            }
+        }
+        return result;
+    }
+
+    public <T> List<T> fourthGeneric(List<T> listMethod, Predicate<T> predicate) {
+        List<T> result = new ArrayList<>();
+        for (T t : listMethod) {
+            if (predicate.test(t)) {
+                result.add(t);
+            }
+        }
+        return result;
+    }
+}
